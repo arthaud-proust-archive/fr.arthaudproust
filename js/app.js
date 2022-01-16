3366474c4047588430c112e39fa66322c4af5e15
Arthaud Proust <arthaudp33@gmail.com>
2022-01-16 17:10:58 +0100

    improve ux, add transitions

--- js/app.js
+++ js/app.js
@@ -1,62 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() {
     const sections = Array.from(document.querySelectorAll('#main > div > section'));
     const sectionEl = document.querySelector('.section-swiper');
+    var sectionSwiper;
     const pageArrows = {
         left: document.querySelector('.pageArrow.left'),
         right: document.querySelector('.pageArrow.right')
     };
-    try {
-    const sectionSwiper = new Swiper(sectionEl, {
-        // Optional parameters
-        direction: 'vertical',
-        grabCursor: true,
-        loop: false,
-        freeMode: false,
-        // autoHeight: true,
-        // resistance: false,
-        mousewheel: {
-            forceToAxis: true,
-            eventsTarget: sectionEl.closest('.swiper-slide')
-        },
-        resistanceRatio: 0.9,
-    });
-    sectionSwiper.on("slideChange", function(e) {
-        // e.activeIndex
-        // try {
-        //     const section = e.el.closest('section');
-        //     const imageCover = document.getElementById('section-image-cover');
-        //     if(!imageCover) return;
-        //     const card = e.el.querySelectorAll('.swiper-slide')[e.activeIndex+1];
-        //     const img = card.dataset.cover;
-        //     if(!img) return;
-        //     imageCover.src = img;
-        // } catch(e) {}
-        try {
-            const imageCoverContainer = document.getElementById('section-image-cover-container');
 
-            const imgs = Array.from(imageCoverContainer.children);
-            imgs.forEach(img=>img.classList.add('d-none'));
 
-            const newI = window.screen.width>576?e.activeIndex+1:e.activeIndex;
-
-            const card = e.el.querySelectorAll('.swiper-slide')[newI];
-            if(!card) throw new Error();
-            const imgName = card.dataset.cover;
-            console.log(imgName);
-            if(!imgName) throw new Error();
-            const img = imageCoverContainer.querySelector(`#sic-${imgName}`);
-            img.classList.remove('d-none');
-        } catch(e) {
-            document.getElementById('section-image-cover-container').querySelector(`#sic-preview`).classList.remove('d-none');
-        }
-    })
-    } catch(e){}
     const mainSwiper = new Swiper('.main-swiper', {
         // Optional parameters
         direction: 'horizontal',
         loop: false,
+        speed: 1500,
         mousewheel: {
-            forceToAxis: true,
+            forceToAxis: false,
             eventsTarget: '.main-swiper'
         },
         parallax: true,
@@ -92,6 +50,93 @@ window.addEventListener('DOMContentLoaded', function() {
             el: '.swiper-scrollbar',
         },
     });
+
+    try {
+        sectionSwiper = new Swiper(sectionEl, {
+            // Optional parameters
+            direction: 'vertical',
+            grabCursor: true,
+            loop: false,
+            freeMode: false,
+            // autoHeight: true,
+            // resistance: false,
+            mousewheel: {
+                forceToAxis: true,
+                eventsTarget: sectionEl.closest('.swiper-slide')
+            },
+            resistanceRatio: 0.9,
+        });
+        sectionSwiper.on("reachBeginning", function(e) {
+            if(mainSwiper.slides.length>0 && !mainSwiper.isBeginning) mainSwiper.slidePrev();
+        });
+        sectionSwiper.on("reachEnd", function(e) {
+            console.log(mainSwiper);
+            if(mainSwiper.slides.length>0 && !mainSwiper.isEnd) mainSwiper.slideNext();
+        });
+        
+        sectionSwiper.on("slideChange", function(e) {
+            console.log(this);
+            // e.activeIndex
+            // try {
+            //     const section = e.el.closest('section');
+            //     const imageCover = document.getElementById('section-image-cover');
+            //     if(!imageCover) return;
+            //     const card = e.el.querySelectorAll('.swiper-slide')[e.activeIndex+1];
+            //     const img = card.dataset.cover;
+            //     if(!img) return;
+            //     imageCover.src = img;
+            // } catch(e) {}
+            try {
+                const imageCoverContainer = document.getElementById('section-image-cover-container');
+
+                const imgs = Array.from(imageCoverContainer.children);
+                imgs.forEach(img=>img.classList.add('d-none'));
+
+                const newI = window.screen.width>576?e.activeIndex+1:e.activeIndex;
+
+                const card = e.el.querySelectorAll('.swiper-slide')[newI];
+                if(!card) throw new Error();
+                const imgName = card.dataset.cover;
+                console.log(imgName);
+                if(!imgName) throw new Error();
+                const img = imageCoverContainer.querySelector(`#sic-${imgName}`);
+                img.classList.remove('d-none');
+            } catch(e) {
+                document.getElementById('section-image-cover-container').querySelector(`#sic-preview`).classList.remove('d-none');
+            }
+        })
+    } catch(e){}
+
+
+    
+    
+
+    const updateMainScroll = function (e) {
+        console.log('szszzzz');
+        refreshPageArrows();
+        if(
+            mainSwiper.slides[mainSwiper.activeIndex].contains(sectionSwiper.el)
+        ){
+            if(sectionSwiper.isBeginning) {
+                console.log('at beginning');
+                try{sectionSwiper.slideNext(0);}catch(e){}
+            } 
+            if(sectionSwiper.isEnd) {
+                console.log('at end');
+                console.log('at beginning');
+                try{sectionSwiper.slidePrev(0);}catch(e){}
+            } 
+            console.log('?');
+            mainSwiper.mousewheel.disable();
+        }else {
+            mainSwiper.mousewheel.enable();
+        }
+    };
+
+    updateMainScroll();
+
+    mainSwiper.on("slideChange", updateMainScroll);
+    
     refreshPageArrows();
 
     function slideToHash(to) {
@@ -108,8 +153,6 @@ window.addEventListener('DOMContentLoaded', function() {
     mainSwiper.on('touchEnd', function () {
         mainSwiper.el.classList.remove('grabbing');
     });
-
-    mainSwiper.on("slideChange", refreshPageArrows);
 
     pageArrows.left.addEventListener('click', function() {
         mainSwiper.slidePrev(1500);
